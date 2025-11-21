7bd159e4d7a224a3f90f78cca09c861da29b7e32
Alex <jalex21@mail.ru>
2025-11-21 14:30:11 +0700

    tool message

--- src/DTO/ToolResultMessage.php
+++ src/DTO/ToolResultMessage.php
@@ -9,7 +9,7 @@ class ToolResultMessage
     public function __construct(
         public string $tool_call_id,
         public string|array|null $content,
-        public string $role = 'assistant',
+        public Role $role = Role::Assistant,
         public string $type = 'tool_result',
     ) {}
 }
